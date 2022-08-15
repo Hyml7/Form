6b4dbfd35db8584474e4de7a65c9c7fe72514658
Hyml7 <110779540+Hyml7@users.noreply.github.com>
2022-08-15 18:20:39 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html> 
+<html>
+ <head> 
+<title>Store form data in .txt file</title> </head>
+ <body>
+ <form method="post"> Enter Your Text Here:<br> 
+<input type="text" name="textdata"><br>
+ <input type="submit" name="submit">
+ </form> 
+</body> 
+</html>
+
+ <?php if(isset($_POST['index'])) { $data=$_POST['index']; $fp = fopen('data.txt', 'a'); fwrite($fp, $data); fclose($fp); } ?>
